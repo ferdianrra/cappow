0052256876f83319133662945dd3bc285dd0556c
Ferdiansyah Annora <ferdianrra@student.gunadarma.ac.id>
2026-06-27 23:35:49 +0700

    fix: resolve merge conflict

--- scenes/character_select/character_select_menu.gd
+++ scenes/character_select/character_select_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _on_people_player_button_pressed() -> void:
 	Global.selected_players = 1
+	#get_tree().change_scene_to_file("res://scenes/test/test_Scene_tiles_map.tscn")
+
 	get_tree().change_scene_to_file("res://scenes/test/game_scene_arena.tscn")
 
 
